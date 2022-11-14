058d835542f5b44451d9160f17e9be4ff9c50117
mrfaptastic <12006953+mrfaptastic@users.noreply.github.com>
2022-11-14 21:32:23 +0000

    Update ArduinoStreamParser.cpp

--- ArduinoStreamParser.cpp
+++ ArduinoStreamParser.cpp
@@ -1,42 +1,27 @@
 #include "ArduinoStreamParser.h"
 
+bool ArudinoStreamParser::parseString(String &data)
+{
+	for (int i = 0; i < data.length(); i++)
+	{
+		parse(data[i]);
+	}
+
+	return true;
+}
 
 size_t ArudinoStreamParser::write(const uint8_t *data, size_t size) {
-	
-	// Trick the Stream Class into thinking it's writing to memory
-	//Serial.println("ArudinoStreamParser::write(data, size)");
-	
-	uint8_t char_val = 0x00;
-	
-    if(size && data) 
-	{
-		for (size_t idx=0; idx<size; idx++)
-		{
-			char_val = *(data + idx);
-			parse(char_val);
-		}
-		
-            return size;
-        
-		
-       // DEBUGV(":ArudinoStreamParser: OOM (%d->%d)\n", size);        
-    }
 	
     return 0;
 }
 
 size_t ArudinoStreamParser::write(uint8_t data) {
-	
-	//Serial.println("ArudinoStreamParser::write(char)");
-
-	// Catch the write of the byte/char from the arduino library.
-	parse(data);
-	
-	return 1;
+		
+	return -1;
 }
 
 int ArudinoStreamParser::available() {
-    return 1;
+    return -1;
 }
 
 int ArudinoStreamParser::read() {
